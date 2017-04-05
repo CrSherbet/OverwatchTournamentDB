b1eb141e9587a8ec3f4bcd84349c064821600b62
Warisara Inprom <warisaracr.y@gmail.com>
2017-04-06 03:34:54 +0700

    first ver.

---
+++ JSFunction.js
@@ -0,0 +1,11 @@
+function getTourPage(){
+    window.open ('Tournament.php','_self',false)
+}
+
+function getHeroPage(){
+    window.open ('Hero.php','_self',false)
+}
+
+function getTeamPage(){
+    window.open ('Tournament.php','_self',false)
+}
