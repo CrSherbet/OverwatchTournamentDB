aa1e350b12f6055316d8857a35efddfe7e56a8c1
yeast <yeast72@gmail.com>
2017-04-21 00:28:24 +0700

    delete file start with big char

--- javascript/JSFunction.js
+++ javascript/JSFunction.js
@@ -1,62 +1,79 @@
 var tierState = 0;
 var price = 0;
 var TIER = {
-    PREMIER : 1,
-    MAJOR : 2,
-    MINOR : 4
+    PREMIER: 1,
+    MAJOR: 2,
+    MINOR: 4
 }
-function getTourPage(){ window.open ('Tournament.html','_self',false)}
-function getTeamPage(){ window.open ('Team.html','_self',false)}
-function getHeroPage(){ window.open ('Hero.html','_self',false)}
-function getMapPage(){ window.open ('Map.html','_self',false)}
-function getPlayerPage(){ window.open ('Player.html','_self',false)}
-function backToMain(){ window.open ('index.html','_self',false)}
 
-function setStateOfTier(){
-    if(document.getElementById("tierPremier").checked)
+function getTourPage() {
+    window.open('tournament.html', '_self', false)
+}
+
+function getTeamPage() {
+    window.open('team.html', '_self', false)
+}
+
+function getHeroPage() {
+    window.open('hero.html', '_self', false)
+}
+
+function getMapPage() {
+    window.open('map.html', '_self', false)
+}
+
+function getPlayerPage() {
+    window.open('player.html', '_self', false)
+}
+
+function backToMain() {
+    window.open('index.html', '_self', false)
+}
+
+function setStateOfTier() {
+    if (document.getElementById("tierPremier").checked)
         tierState += TIER.PREMIER;
-    if(document.getElementById("tierMajor").checked)
+    if (document.getElementById("tierMajor").checked)
         tierState += TIER.MAJOR;
-    if(document.getElementById("tierMinor").checked)
+    if (document.getElementById("tierMinor").checked)
         tierState += TIER.MINOR;
 }
 
-function setPrice(){
+function setPrice() {
     var priceItem = document.getElementById("price");
     price = priceItem.options[priceItem.selectedIndex].value;
 }
 
-function searchOpTour(){
+function searchOpTour() {
     setStateOfTier();
     setPrice();
-    alert(tierState+" "+price);
-     var values =  {
-       'key'    : "searchOpTour",
-       'tierState'    : tierState,
-       'price'    : price
+    alert(tierState + " " + price);
+    var values = {
+        'key': "searchOpTour",
+        'tierState': tierState,
+        'price': price
     }
     sendRequest(values);
     tierState = 0;
-
 }
 
-function searchTourName(){
+function searchTourName() {
     var val = document.getElementById("tourNameText").value;
-    var values =  {
-       'key'    : "searchTourName",
-       'name'   : val
+    var values = {
+        'key': "searchTourName",
+        'name': val
     }
     sendRequest(values);
 }
 
-function sendRequest(values){
+function tourSendRequest(values) {
     var result = $.ajax({
-        url: "PHPFunction.php",
+        url: "tourPHPFn.php",
         type: "post",
         data: values,
-        success: function(data,status,xhr){
-           $("#content").html(JSON.stringify(data));
-           console.log(JSON.stringify(data));
+        success: function (data, status, xhr) {
+            $("#content").html(JSON.stringify(data));
+            console.log(JSON.stringify(data));
         },
         error: function (request, status, error) {
             console.log(error);
