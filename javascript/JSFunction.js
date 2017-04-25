fc699f5459709f2ae56e9c6aafbe624006d30da4
yeast <yeast72@gmail.com>
2017-04-25 09:18:53 +0700

    debug1

--- javascript/JSFunction.js
+++ javascript/JSFunction.js
@@ -7,30 +7,6 @@ var TIER = {
     MINOR: 4
 }
 
-function getTourPage() {
-    window.open('tournament.html', '_self', false)
-}
-
-function getTeamPage() {
-    window.open('team.html', '_self', false)
-}
-
-function getHeroPage() {
-    window.open('hero.html', '_self', false)
-}
-
-function getMapPage() {
-    window.open('map.html', '_self', false)
-}
-
-function getPlayerPage() {
-    window.open('player.html', '_self', false)
-}
-
-function backToMain() {
-    window.open('index.html', '_self', false)
-}
-
 function setStateOfTier() {
     if (document.getElementById("tierPremier").checked)
         tierState += TIER.PREMIER;
@@ -116,4 +92,19 @@ function searchPlayerName(){
 function dropdown(val) {
     var y = document.getElementsByClassName('btn btn-default dropdown-toggle');
     var aNode = y[0].innerHTML = val +' <span class="caret"></span>';
+}
+
+function sendRequest(values) {
+    var result = $.ajax({
+        url: phpURL,
+        type: "post",
+        data: values,
+        success: function (data, status, xhr) {
+            $("#content").html(JSON.stringify(data));
+            console.log(JSON.stringify(data));
+        },
+        error: function (request, status, error) {
+            console.log(error);
+        }
+    });
 }
